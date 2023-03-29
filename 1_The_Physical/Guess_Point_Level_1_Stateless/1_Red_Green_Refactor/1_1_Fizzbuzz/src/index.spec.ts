78681445c6a5b1993f4f1fa3c27a5a2e3f9da915
Taha <taha@marketer.ly>
2023-03-29 12:16:21 +0200

    [new] fizzBuzz rejects numbers above 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,16 +1,19 @@
 import { fizzBuzz } from './fizzbuzz';
 
 describe('fizzbuzz', () => {
+  it("doesn't accepts -1 as an argument because it's not between 1 and 100", () => {
+    expect(() => fizzBuzz(-1)).toThrow('Too small number');
+  });
+
+  it("doesn't accepts 101 as an argument because it's more than 100", () => {
+    expect(() => fizzBuzz(101)).toThrow('Too large number');
+  });
   it('returns a string', () => {
     expect(typeof fizzBuzz(5)).toBe('string');
   });
 
   it('returns Fizz for 3', () => {
     expect(fizzBuzz(3)).toEqual('Fizz');
-  });
-
-  it("doesn't accepts 0 as an argument because it's not between 1 and 100", () => {
-    expect(() => fizzBuzz(0)).toThrow('Too small number');
   });
 
   it('returns Buzz for multiples of 5', () => {
@@ -20,7 +23,7 @@ describe('fizzbuzz', () => {
   });
 
   it('returns FizzBuzz for multipls of both 3 & 5', () => {
-    let multiples = [15, 30, 45, 60, 75, 90, 105, 120, 135, 150];
+    let multiples = [15, 30, 45, 60, 75, 90];
     multiples
       .map(multiple => fizzBuzz(multiple))
       .forEach(m => expect(m).toEqual('FizzBuzz'));
