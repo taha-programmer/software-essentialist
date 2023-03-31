e77b1be4eee28f50607c65b64a4bcde1bfc951a1
Taha <taha@marketer.ly>
2023-03-31 09:43:32 +0200

    [refactor]: Organize the spec file (add 1 test)

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -5,27 +5,39 @@ describe('palindrome checker', () => {
 
   beforeEach(() => (palindromeChecker = new PalindromeChecker()));
 
-  it('should exist', () => {
-    expect(palindromeChecker).toBeDefined();
+  describe('Basic Tests', () => {
+    it('should exist', () => {
+      expect(palindromeChecker).toBeDefined();
+    });
+
+    it('should return boolean', () => {
+      expect(typeof palindromeChecker.isAPalindrome('')).toBe('boolean');
+    });
   });
 
-  it('should return boolean', () => {
-    expect(typeof palindromeChecker.isAPalindrome('')).toBe('boolean');
+  describe('Single words tests', () => {
+    it("should know that 'mom' is a palindrome", () => {
+      expect(palindromeChecker.isAPalindrome('mom')).toBe(true);
+    });
+
+    it("should know that 'bill' is NOT a palindrome", () => {
+      expect(palindromeChecker.isAPalindrome('bill')).toBeFalsy();
+    });
+
+    it('should know that "Mom" is also a palindrome', () => {
+      expect(palindromeChecker.isAPalindrome('Mom')).toBeTruthy();
+    });
   });
 
-  it("should know that 'mom' is a palindrome", () => {
-    expect(palindromeChecker.isAPalindrome('mom')).toBe(true);
-  });
+  describe('Sentences Tests', () => {
+    it('should know that "Was It A Rat I Saw" is a palindrome', () => {
+      expect(
+        palindromeChecker.isAPalindrome('Was It A Rat I Saw')
+      ).toBeTruthy();
+    });
 
-  it("should know that 'bill' is NOT a palindrome", () => {
-    expect(palindromeChecker.isAPalindrome('bill')).toBeFalsy();
-  });
-
-  it('should know that "Mom" is also a palindrome', () => {
-    expect(palindromeChecker.isAPalindrome('Mom')).toBeTruthy();
-  });
-
-  it('should know that "Was It A Rat I Saw" is a palindrome', () => {
-    expect(palindromeChecker.isAPalindrome('Was It A Rat I Saw')).toBeTruthy();
+    it('should know that "Never Odd or Even" is a palindrome', () => {
+      expect(palindromeChecker.isAPalindrome('Never Odd or Even')).toBeTruthy();
+    });
   });
 });
