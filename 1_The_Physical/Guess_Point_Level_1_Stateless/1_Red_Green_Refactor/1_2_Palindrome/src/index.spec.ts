73e935df8b82bf5d7e2953d219ff2c8bbec870b3
Taha <taha@marketer.ly>
2023-03-31 08:12:38 +0200

    [new]: it knows that 'bill' is not a palindrome

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -16,4 +16,8 @@ describe('palindrome checker', () => {
   it("should know that 'mom' is a palindrome", () => {
     expect(palindromeChecker.isAPalindrome('mom')).toBe(true);
   });
+
+  it("should know that 'bill' is NOT a palindrome", () => {
+    expect(palindromeChecker.isAPalindrome('bill')).toBeFalsy();
+  });
 });
