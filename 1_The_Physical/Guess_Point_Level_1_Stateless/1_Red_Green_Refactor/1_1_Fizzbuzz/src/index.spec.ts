b6bd6b82f4fcc17fd7077ec9c664d4a6cc3124aa
Taha <taha@marketer.ly>
2023-05-20 16:17:48 +0200

    [refactor] organize test code in describe blocks

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,8 +1,22 @@
 import { fizzbuzz } from './fizzbuzz';
 
 describe('fizzbuzz', () => {
-  it('returns a string', () => {
-    expect(typeof fizzbuzz(1)).toBe('string');
+  describe('Accept only input of type number, between 1 and 100, and returns string', () => {
+    it('throws an error when given non-number input', () => {
+      expect(() => fizzbuzz('hello' as any)).toThrow();
+    });
+
+    it('throws an error when given -12', () => {
+      expect(() => fizzbuzz(-12)).toThrow();
+    });
+
+    it('throws an error when given 102', () => {
+      expect(() => fizzbuzz(102)).toThrow();
+    });
+
+    it('returns a string', () => {
+      expect(typeof fizzbuzz(1)).toBe('string');
+    });
   });
 
   describe('return "Fizz" when given a multiple of 3', () => {
@@ -43,17 +57,5 @@ describe('fizzbuzz', () => {
 
   it('returns "43" when given 43', () => {
     expect(fizzbuzz(43)).toBe('43');
-  });
-
-  it('throws an error when given 102', () => {
-    expect(() => fizzbuzz(102)).toThrow();
-  });
-
-  it('throws an error when given -12', () => {
-    expect(() => fizzbuzz(-12)).toThrow();
-  });
-
-  it('throws an error when given non-number input', () => {
-    expect(() => fizzbuzz('hello' as any)).toThrow();
   });
 });
