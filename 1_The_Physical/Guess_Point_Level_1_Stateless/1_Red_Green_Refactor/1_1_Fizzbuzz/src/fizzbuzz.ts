7f003ab7d1228cf504bd1fbdd20bb2be23b5d9e8
Taha <taha@marketer.ly>
2023-05-20 15:36:27 +0200

    [green] Throw an error for -12

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export const fizzbuzz = (num: number): string => {
   if (num > 100) throw new Error('Number must be less than 100');
+  if (num < 0) throw new Error('Number must be greater than 0');
   if (num === 43) return '43';
   const isMultipleOfThreeAndFive = num % 3 === 0 && num % 5 === 0;
   if (isMultipleOfThreeAndFive) return 'FizzBuzz';
