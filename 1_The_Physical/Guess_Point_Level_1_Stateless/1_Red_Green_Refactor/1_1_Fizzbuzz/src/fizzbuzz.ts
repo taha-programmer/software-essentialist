6a2789b0e1a029251380acd9da36e9d72ba48f80
Taha <taha@marketer.ly>
2023-05-20 14:28:50 +0200

    [green] return 'FizzBuzz' for 45

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,7 @@
 export const fizzbuzz = (num: number): string => {
   if (num === 43) return '43';
   if (num === 15) return 'FizzBuzz';
+  if (num === 45) return 'FizzBuzz';
   const isMultipleOfThree = num % 3 === 0;
   if (isMultipleOfThree) return 'Fizz';
   return 'Buzz';
