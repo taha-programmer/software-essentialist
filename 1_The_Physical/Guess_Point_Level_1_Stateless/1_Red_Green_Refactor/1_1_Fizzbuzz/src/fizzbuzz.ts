1dcf6ff25161c3cc93e53f6c273fbd47e0b45e0a
Taha <taha@marketer.ly>
2023-05-21 10:35:05 +0200

    [refactor] improvement to fizzbuzz function

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,10 +2,14 @@ export const fizzbuzz = (num: number): string => {
   if (typeof num !== 'number') throw new Error('Input must be a number');
   if (num > 100) throw new Error('Number must be less than 100');
   if (num < 0) throw new Error('Number must be greater than 0');
-  if (num === 43) return '43';
+
   const isMultipleOfThreeAndFive = num % 3 === 0 && num % 5 === 0;
-  if (isMultipleOfThreeAndFive) return 'FizzBuzz';
   const isMultipleOfThree = num % 3 === 0;
+  const isMultipleOfFive = num % 5 === 0;
+
+  if (isMultipleOfThreeAndFive) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
-  return 'Buzz';
+  if (isMultipleOfFive) return 'Buzz';
+
+  return num.toString();
 };
