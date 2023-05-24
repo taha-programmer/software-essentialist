60d0e1ade80ce0bbbb3d83f2df5fb2b4b9242b4c
Taha <taha@marketer.ly>
2023-05-24 14:22:29 +0200

    [red/green] makes the following tests pass:
    test for 'Mom' and 'xMomx'

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,6 @@
 export class PalindromeChecker {
   public isPalindrome = (input: string): boolean => {
     const reversed = input.split('').reverse().join('');
-    return input === reversed;
+    return input.toLowerCase() === reversed.toLowerCase();
   };
 }
