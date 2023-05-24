0de94c7b8168831cc3b12d8ae0eb73c88980fae5
Taha <taha@marketer.ly>
2023-05-24 06:31:17 +0200

    [refactor] fix the range checking logic

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,7 +1,7 @@
 export const fizzbuzz = (num: number): string => {
   if (typeof num !== 'number') throw new Error('Input must be a number');
   if (num > 100) throw new Error('Number must be less than 100');
-  if (num < 0) throw new Error('Number must be greater than 0');
+  if (num < 1) throw new Error('Number must be greater than 0');
 
   const isMultipleOfThreeAndFive = num % 3 === 0 && num % 5 === 0;
   const isMultipleOfThree = num % 3 === 0;
