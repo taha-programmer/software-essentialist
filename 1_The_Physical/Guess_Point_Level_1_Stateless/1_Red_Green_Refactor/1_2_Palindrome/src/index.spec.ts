9d398a01338e3ab30e9ebec1fb372b53fd10d2cb
Taha <taha@marketer.ly>
2023-05-24 14:31:05 +0200

    [green] it knows that 'Never Od..' is a palindrome

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -31,4 +31,8 @@ describe('palindrome checker', () => {
   it('knows that "bill" is NOT a palindrome', () => {
     expect(palindromeChecker.isPalindrome('bill')).toBe(false);
   });
+
+  it('knows that "Never Odd or Even" is a palindrome', () => {
+    expect(palindromeChecker.isPalindrome('Never Odd or Even')).toBe(true);
+  });
 });
