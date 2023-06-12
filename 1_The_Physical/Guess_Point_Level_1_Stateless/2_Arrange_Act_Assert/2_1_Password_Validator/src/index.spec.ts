79bfc93321dabd7bbee9de1e4a2aff7b5915cb16
Taha <taha@marketer.ly>
2023-06-12 14:37:00 +0200

    refactor: Combine tests into a "describe" block
    combine the tests of the same behaviour into a describe block

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,21 +1,27 @@
 import { PasswordValidator } from './index';
 
 describe('password validator', () => {
-  it('knows that "watermelon" is valid because its between 5 & 15 charachters long', () => {
-    let resultObject = PasswordValidator.check('watermelon');
-    expect(resultObject.result).toBeTruthy();
-    expect(resultObject.errors.length).toBe(0);
-  });
+  describe('Accepts passwords that are between 5 & 15 chars length', () => {
+    it('knows that "watermelon" is valid because its between 5 & 15 charachters long', () => {
+      let resultObject = PasswordValidator.check('watermelon');
+      let errorsLength = resultObject.errors.length;
 
-  it('knows that "thinking" is valid because its between 5 & 15 charachters long', () => {
-    let resultObject = PasswordValidator.check('thinking');
-    expect(resultObject.result).toBeTruthy();
-    expect(resultObject.errors.length).toBe(0);
-  });
+      expect(resultObject.result).toBeTruthy();
+      expect(resultObject.errors.length).toBe(errorsLength);
+    });
 
-  it('knows that "mom" is NOT valid because it\'s NOT between 5 & 15 charachters long', () => {
-    let resultObject = PasswordValidator.check('mom');
-    expect(resultObject.result).toBeFalsy();
-    expect(resultObject.errors.length).toBe(1);
+    it('knows that "thinking" is valid because its between 5 & 15 charachters long', () => {
+      let resultObject = PasswordValidator.check('thinking');
+      let errorsLength = resultObject.errors.length;
+      expect(resultObject.result).toBeTruthy();
+      expect(resultObject.errors.length).toBe(errorsLength);
+    });
+
+    it('knows that "mom" is NOT valid because it\'s NOT between 5 & 15 charachters long', () => {
+      let resultObject = PasswordValidator.check('mom');
+      let errorsLength = resultObject.errors.length;
+      expect(resultObject.result).toBeFalsy();
+      expect(resultObject.errors.length).toBe(errorsLength);
+    });
   });
 });
