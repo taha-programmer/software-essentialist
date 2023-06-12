8de5c9a3e7fd64da064cbdaebef3585626ddcd9a
Taha <taha@marketer.ly>
2023-06-12 14:00:59 +0200

    green: it acceptss 'watermelon'
    Because it's between 5 & 15 chars long

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,11 @@
+import { PasswordValidator } from './index';
 
 describe('password validator', () => {
+  it('knows that "watermelon" is valid because its between 5 & 15 charachters long', () => {
+    let resultObject = PasswordValidator.check('watermelon');
+    expect(resultObject.result).toBeTruthy();
+    expect(resultObject.errors.length).toBe(0);
+  });
 
 })
 
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -0,0 +1,8 @@
+export class PasswordValidator {
+  public static check(input: string) {
+    return {
+      result: true,
+      errors: [],
+    };
+  }
+}
