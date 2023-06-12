3f2afb32275377065a250f8af7be7e444ea6d9ba
Taha <taha@marketer.ly>
2023-06-12 15:18:53 +0200

    refactor: use it.each() for parameterization

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,28 +1,19 @@
 import { PasswordValidator } from './index';
 
 describe('password validator', () => {
-  describe('Accepts passwords that are between 5 & 15 chars length', () => {
-    it('knows that "watermelon" is valid because its between 5 & 15 charachters long', () => {
-      let resultObject = PasswordValidator.check('watermelon');
-      let errorsLength = resultObject.errors.length;
-
-      expect(resultObject.result).toBeTruthy();
-      expect(resultObject.errors.length).toBe(errorsLength);
-    });
-
-    it('knows that "thinking" is valid because its between 5 & 15 charachters long', () => {
-      let resultObject = PasswordValidator.check('thinking');
-      let errorsLength = resultObject.errors.length;
-      expect(resultObject.result).toBeTruthy();
-      expect(resultObject.errors.length).toBe(errorsLength);
-    });
-
-    it('knows that "mom" is NOT valid because it\'s NOT between 5 & 15 charachters long', () => {
-      let resultObject = PasswordValidator.check('mom');
-      let errorsLength = resultObject.errors.length;
-      expect(resultObject.result).toBeFalsy();
-      expect(resultObject.errors.length).toBe(errorsLength);
-      expect(resultObject.errors[0]).toBe('InvalidLength');
-    });
+  describe('Accepts passwords that are between 5 & 15 chars long', () => {
+    it.each([
+      ['watermelon', true, []],
+      ['thinking', true, []],
+      ['mom', false, ['InvalidLength']],
+    ])(
+      'knows that %s should return %s',
+      (input: string, result: boolean, errors: string[]) => {
+        let resultObject = PasswordValidator.check(input);
+        expect(resultObject.result).toBe(result);
+        expect(resultObject.errors).toHaveLength(errors.length);
+        expect(resultObject.errors).toEqual(errors);
+      }
+    );
   });
 });
