692605faaee65eeb6cb36c53ced286c9506675c4
Taha <taha@marketer.ly>
2023-06-12 14:02:40 +0200

    green: it accepts 'thinking' as a password
    Because it's between 5 & 15 chars long

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -7,5 +7,9 @@ describe('password validator', () => {
     expect(resultObject.errors.length).toBe(0);
   });
 
-})
+  it('knows that "thinking" is valid because its between 5 & 15 charachters long', () => {
+    let resultObject = PasswordValidator.check('thinking');
+    expect(resultObject.result).toBeTruthy();
+    expect(resultObject.errors.length).toBe(0);
+  });
 
