9ad7d10528d1170860da061ac98d84a3e7b69d41
Taha <taha@marketer.ly>
2023-06-14 13:11:43 +0200

    [new] another example for finding the Minimum

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -2,6 +2,7 @@ import { StatsCalculator } from './index';
 
 describe('stats calculator', () => {
   const firstSequence = [13, 3, 7, 15, 11, 5, 9];
+  const secondSequence = [601, 101, 401, 701, 501, 201, 301];
   it('exists', () => {
     expect(StatsCalculator).toBeDefined();
   });
@@ -20,9 +21,14 @@ describe('stats calculator', () => {
   });
 
   describe('Calculate minimum value in a sequence', () => {
-    it(`knows that 3 is the mimum value in this sequence ${firstSequence}`, () => {
+    it(`knows that 3 is the minimum value in this sequence ${firstSequence}`, () => {
       let result = StatsCalculator.calculateStats(firstSequence);
       expect(result.minimum).toBe(3);
+    });
+
+    it(`knows that 101 is the minimum value in this sequence ${secondSequence}`, () => {
+      let result = StatsCalculator.calculateStats(secondSequence);
+      expect(result.minimum).toBe(101);
     });
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -6,8 +6,13 @@ type statistics = {
 };
 export class StatsCalculator {
   public static calculateStats = (input: number[]): statistics => {
+    let minimum: number = input[0];
+    for (let index = 0; index < input.length; index++) {
+      minimum = input[index] < minimum ? input[index] : minimum;
+    }
+
     return {
-      minimum: 3,
+      minimum,
       maximum: 1,
       count: 1,
       average: 1,
