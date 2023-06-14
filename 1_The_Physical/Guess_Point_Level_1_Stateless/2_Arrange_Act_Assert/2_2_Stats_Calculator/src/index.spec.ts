5af12beac3d401824f0a222316e01e2976158687
Taha <taha@marketer.ly>
2023-06-14 11:56:30 +0200

    [new] returns an object with 4 statistical keys

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -9,5 +9,13 @@ describe('stats calculator', () => {
     const result = StatsCalculator.calculateStats([3, 4, 5, 6]);
     expect(typeof result).toBe('object');
   });
+
+  it('returns an object with 4 statistical keys', () => {
+    const result = StatsCalculator.calculateStats([1]);
+    expect(result).toHaveProperty('minimum');
+    expect(result).toHaveProperty('maximum');
+    expect(result).toHaveProperty('count');
+    expect(result).toHaveProperty('average');
+  });
 });
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,5 +1,16 @@
+type statistics = {
+  minimum: number;
+  maximum: number;
+  count: number;
+  average: number;
+};
 export class StatsCalculator {
-  public static calculateStats = (input: number[]): object => {
-    return {};
+  public static calculateStats = (input: number[]): statistics => {
+    return {
+      minimum: 1,
+      maximum: 1,
+      count: 1,
+      average: 1,
+    };
   };
 }
